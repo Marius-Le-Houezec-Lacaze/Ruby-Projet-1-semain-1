10d92493af44baae9e452837f75a691f8fbbbb30
mariuslhl <mariuslhl@gmail.com>
2020-01-13 12:05:33 +0100

    Initial commit to this repo

---
+++ exo_03.rb
@@ -0,0 +1,3 @@
+# coding: utf-8
+puts "Bonjour , monde !" #puts Rajoute un retour a la ligne a la fin du text
+print "Et avec une voix sexy, ça donne : Bonjour, monde !" #print ne rajoute pas de retour a la ligne
