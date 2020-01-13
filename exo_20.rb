768b222d1bd54506881e10b9e954169f5d493f1f
mariuslhl <mariuslhl@gmail.com>
2020-01-13 16:46:40 +0100

    Pushin exo 20

---
+++ exo_20.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print ">"
+
+etage = gets.chomp.to_i
+collum = etage + 1
+i = 0
+row = 0
+indice = etage
+
+while i < etage
+  print "#"
+  while row != 0
+    row = row - 1
+    print "#"
+  end
+  puts" "
+  i = i + 1
+  row = i
+end
