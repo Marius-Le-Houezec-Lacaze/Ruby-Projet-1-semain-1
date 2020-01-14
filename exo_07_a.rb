809965e3d6d72db6bb521e12ed242acefa27ca11
mariuslhl <mariuslhl@gmail.com>
2020-01-14 11:24:07 +0100

    minor correction to exo 07

--- exo_07_a.rb
+++ exo_07_a.rb
@@ -4,5 +4,5 @@ user_name = gets.chomp
 puts user_name
 
 # puts demande a l'utilisateur son nom 
-# gets.chomp sers a demander a l'utilisateur d'entrer des donner pars le terminal
+# gets.chomp sers a recuperer du text pars le terminal
 # ici ils sert a demander le nom pour ensuit l'afficher
